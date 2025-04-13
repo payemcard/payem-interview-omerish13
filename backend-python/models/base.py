3ef9b2646623791560022aa77f3c2a8faa9f4b22
omerish13 <omerishai@gmail.com>
2025-04-12 20:36:17 -0400

    add custom JSON decoder to handle request_type conversion

--- backend-python/models/base.py
+++ backend-python/models/base.py
@@ -11,6 +11,18 @@ class CustomJSONEncoder(json.JSONEncoder):
             return obj.value
         return super().default(obj)
 
+
+def object_hook(obj):
+    if 'request_type' in obj:
+        obj['request_type'] = RequestType(obj['request_type'])
+    return obj
+
+
+class CustomJSONDecoder(json.JSONDecoder):
+    def __init__(self, *args, **kwargs):
+        super().__init__(object_hook=object_hook, *args, **kwargs)
+
+
 class RequestType(str, Enum):
     PURCHASE = "Purchase"
     REIMBURSEMENT = "Reimbursement"
